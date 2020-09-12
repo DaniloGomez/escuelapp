9a775e116b1eaa87bffb78a30320e4ba0e48a04f
DaniloGomez <danilogomez8@hotmail.com>
2020-09-11 23:10:38 -0500

    first commit

--- src/recursion/Palindrome.java
+++ src/recursion/Palindrome.java
@@ -4,31 +4,24 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		
-		int[] arr = new int[] {2,3,3,3,3,2};
+		int[] arr = new int[] {2,3,3,1,3,3,2};
 		int x = getPalindrome(arr, 0, arr.length-1);
+		
 		System.out.println("Result: " + x);
 
 	}
 	
 public static int getPalindrome(int[] arr, int from, int until) {
 		
-		int half;
-
-		if(from == until) {
-			System.out.print(arr[from] + " ");
-			return arr[from];
-		}
-		
-		if(arr[from] == arr[until]) {
-			
-			half = getPalindrome(arr, from+1, until);
-			return 1;
-		}
-		
-		else {
-			return 0;
-		}
-
+	if(from == until) {
+		return 1;
 	}
+	
+	if(arr[from] != arr[until]) {
+		return 0 * getPalindrome(arr, from+1, until-1);
+	}
+		return getPalindrome(arr, from+1, until-1);
+
+}
 
 }
